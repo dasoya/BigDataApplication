7f88b38cb072dd93a05e75b8e4c864d7ddefa131
Yujin <yudility@gmail.com>
2023-10-31 18:49:18 +0900

    Feat: Home 화면 ranking top 3 보여주기 기능 구현

--- ranking/cityRanking.php
+++ ranking/cityRanking.php
@@ -1,6 +1,6 @@
 <?php
 
-    $dblink = mysqli_connect("localhost", 'root', '', 'dbName');
+    $dblink = mysqli_connect("localhost", 'root', '', 'bibibig');
     
     if (mysqli_connect_errno()) {
         printf('', mysqli_connect_error());
@@ -8,32 +8,32 @@
     }
     else{
         //Query 내용은 아래 서술
-        $sql = "SELECT c.id, c.name, t.cou
+        $sql = "SELECT c.id, c.city_name, c.info, c.latitude, c.longitude, l.img landmarkImgUrl ,RANK() OVER (ORDER BY t.cou DESC) AS rank
                 FROM (SELECT city_id, COUNT(*) cou
                         FROM trip
                         GROUP BY city_id
                         ORDER BY cou DESC
-                        LIMIT 3) AS t
-                JOIN city AS c ON c.id = t.city_id
-                ORDER BY t.cou desc;";
+                        LIMIT 10) AS t
+                JOIN city AS c ON c.id = t.city_id 
+                JOIN landmark As l ON c.id = l.city_id;";
         
         //데이터를 가져오기만 하면 되는거라 bind_param 사용 안하였음.
         $result = mysqli_query($dblink, $sql);
+        $cityRanking = array();
         while ($row = mysqli_fetch_assoc($result)) {
-            
-            // 출력 구조 어떻게 할지 고민해보기 
+            $cityRanking[] = $row;
         }
         mysqli_free_result($result);
     }
 
     mysqli_close($dblink);
     /*
-    1. 서브쿼리 -> 상위 3개만 가져오기
+    1. 서브쿼리 
     SELECT city_id, count(*) cou
     FROM trip
     GROUP BY city_id
-    ORDER BY cou DESC
-    LIMIT 3
+    ORDER BY cou DESC 
+    LIMIT 10 -- 쿼리 실행 시간 단축을 위해서 추가
 
     2. 메인 쿼리 -> city 테이블에 join해서 name 가져오기
     SELECT c.id, c.name, t.cou
@@ -41,7 +41,7 @@
         FROM trip
         GROUP BY city_id
         ORDER BY cou DESC
-        LIMIT 3) AS t
+        LIMIT 10) AS t
     JOIN city AS c ON c.id = t.city_id
     ORDER BY t.cou desc;
     */
