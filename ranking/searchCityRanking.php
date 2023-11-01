31ffdd2552d0b9b7cad6e4f0cfe828eeb4cf4c05
Yujin <yudility@gmail.com>
2023-11-01 19:06:06 +0900

    Feat: city이름으로 랭킹 검색하는 기능 추가

--- ranking/searchCityRanking.php
+++ ranking/searchCityRanking.php
@@ -7,13 +7,18 @@
         exit();
     }
     // 랭킹 순으로 정리한 테이블에 where문 추가
-    $sql = "SELECT c.id, c.city_name, t.cou, RANK() OVER (ORDER BY t.cou DESC) AS rank
-            FROM (SELECT city_id, COUNT(*) cou
+    $sql = "WITH RankedCities AS (
+                SELECT c.id, c.name, t.cou, RANK() OVER (ORDER BY t.cou DESC) AS rank
+                FROM (
+                    SELECT city_id, COUNT(*) cou
                     FROM trip
                     GROUP BY city_id
-                    ORDER BY cou DESC) AS t
-            JOIN city AS c ON c.id = t.city_id
-            WHERE c.city_name = ? ";
+                ) AS t
+                JOIN city AS c ON c.id = t.city_id
+            )
+            SELECT id, name, cou, rank
+            FROM RankedCities
+            WHERE name = ?;";
     
     if($stmt = mysqli_prepare($dblink, $sql)) {
 
@@ -24,9 +29,10 @@
         if(mysqli_stmt_execute($stmt)) { 
             
             $result = mysqli_stmt_get_result($stmt);
-
+            $searchResult = [];
             while($row = mysqli_fetch_assoc($result)) {
-                //어떻게 띄울지 고민해보기
+                $searchResult[] = $row;
+                echo''. $row['id'] .''. $row['name'].''. $row['cou'] .''. $row['rank'];
             }
             mysqli_free_result($result);
         }
