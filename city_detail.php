a256a689adc821a722ed41991a93f336b01fb004
penguin1109 <1109penguin@gmail.com>
2023-11-07 22:45:02 +0900

    [1107]_only_necessary_files

--- city_detail.php
+++ city_detail.php
@@ -1,7 +1,9 @@
-
-
 <?php
-    session_start();
+// 발급된 세션 id가 있다면 세션의 id를, 없다면 false 반환
+   if(!session_id()) {
+       // id가 없을 경우 세션 시작
+       session_start();
+   };
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- sql/dbCreate.sql
+++ sql/dbCreate.sql
@@ -1,19 +1,21 @@
--- 11/01 카톡 내용 반영
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` integer PRIMARY KEY,
-  `email` varchar(255) UNIQUE COMMENT '회원가입 및 로그인에 사용',
-  `pw` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `age` TINYINT,
-  `sex` ENUM ('Female', 'Male') COMMENT 'Female / Male'
+  `sex` char(1)  COMMENT 'M / W',
+  `nation` char(2),
+  `email` char,
+  `created_at` timestamp COMMENT '필요없으면 빼기. 로그 같은 곳에 쓸 데 없으려나?'
 );
 
 CREATE TABLE `prediction` (
   `id` integer PRIMARY KEY,
+  `start_date` date NOT NULL,
+  `end_date` date,
   `duration` integer,
   `created_at` timestamp COMMENT '기록에 추가한 날짜. 나중에 정렬할 때 사용하면 좋을 듯.',
-  `transportation_type` varchar(255),
-  `transportation_cost` integer,
+  `transportaion_type` varchar(255),
+  `transportaion_cost` integer,
   `accommodation_type` varchar(255),
   `accommodation_cost` integer,
   `user_id` integer NOT NULL,
@@ -24,27 +26,25 @@ CREATE TABLE `trip` (
   `id` integer PRIMARY KEY,
   `travelerName` varchar(255),
   `travelerAge` integer,
-  `travelerSex` ENUM ('Female', 'Male') COMMENT 'Female / Male',
+  `travelerSex` char(1) COMMENT 'M / W',
   `start_date` date,
   `end_date` date,
   `duration` integer,
-  `transportation_type` varchar(255),
-  `transportation_cost` integer,
+  `transportaion_type` varchar(255),
   `accommodation_type` varchar(255),
-  `accommodation_cost` integer,
   `city_id` integer NOT NULL
 );
 
 CREATE TABLE `Userliked` (
   `user_id` integer NOT NULL,
   `city_id` integer,
+  `landmark_id` integer,
   PRIMARY KEY (`user_id`, `city_id`)
 );
 
 CREATE TABLE `country` (
-  `iso_number` integer(3),
-  `iso_code3` char(3),
-  `iso_code2` char(2) PRIMARY KEY,
+  `iso_num2` char(2) PRIMARY KEY,
+  `iso_num3` char(3),
   `name` varchar(255) NOT NULL,
   `flag` text COMMENT '국기 img url',
   `info` text COMMENT '국가 정보',
@@ -57,14 +57,15 @@ CREATE TABLE `review` (
   `body` text COMMENT '리뷰 내용',
   `created_at` timestamp COMMENT '업로드한 날짜 ',
   `img` text COMMENT 'image를 추가할 수 있도록?',
-  `user_id` integer NOT NULL
+  `user_id` integer NOT NULL,
+  `city_id` integer NOT NULL
 );
 
 CREATE TABLE `airport` (
   `iata_code` varchar(32) PRIMARY KEY,
   `icao_code` varchar(32) UNIQUE,
   `airport_name` varchar(255) NOT NULL,
-  `city_id` integer NOT NULL,
+  `continent_id` integer NOT NULL,
   `country_id` char(2) NOT NULL
 );
 
@@ -76,7 +77,7 @@ CREATE TABLE `station` (
 
 CREATE TABLE `city` (
   `id` integer PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
+  `city_name` varchar(255) NOT NULL,
   `latitude` float,
   `longitude` float,
   `info` text COMMENT '도시 설명',
@@ -85,7 +86,7 @@ CREATE TABLE `city` (
 
 CREATE TABLE `landmark` (
   `id` integer PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
+  `landmark_name` varchar(255) NOT NULL,
   `img` text COMMENT 'img url',
   `info` text COMMENT '랜드마크 설명',
   `city_id` integer NOT NULL
@@ -98,32 +99,36 @@ CREATE TABLE `continent` (
 
 CREATE TABLE `feedback` (
   `user_id` integer PRIMARY KEY,
-  `rating` integer NOT NULL,
-  `message` text
+  `rate` integer,
+  `text` text
 );
 
-ALTER TABLE `airport` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (`nation`) REFERENCES `country` (`iso_num2`);
 
-ALTER TABLE `prediction` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `prediction` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 ALTER TABLE `prediction` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
 
 ALTER TABLE `trip` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
 
-ALTER TABLE `Userliked` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `Userliked` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 ALTER TABLE `Userliked` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
 
+ALTER TABLE `Userliked` ADD FOREIGN KEY (`landmark_id`) REFERENCES `landmark` (`id`);
+
 ALTER TABLE `country` ADD FOREIGN KEY (`continent_id`) REFERENCES `continent` (`id`);
 
-ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `airport` ADD FOREIGN KEY (`country_id`) REFERENCES `country` (`iso_code2`);
+ALTER TABLE `review` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
+
+ALTER TABLE `airport` ADD FOREIGN KEY (`country_id`) REFERENCES `country` (`iso_num2`);
 
 ALTER TABLE `station` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
 
-ALTER TABLE `city` ADD FOREIGN KEY (`country_id`) REFERENCES `country` (`iso_code2`);
+ALTER TABLE `city` ADD FOREIGN KEY (`country_id`) REFERENCES `country` (`iso_num2`);
 
 ALTER TABLE `landmark` ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
 
-ALTER TABLE `feedback` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `feedback` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
